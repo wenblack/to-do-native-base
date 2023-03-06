5f711c9780735c92d80ca0e3029085429e73e8b1
Wender Barbosa <wenblack012@gmail.com>
2023-03-06 11:57:55 -0300

    ALtered logic of colorSwitch function on Menu

--- src/components/MenuRight/index.tsx
+++ src/components/MenuRight/index.tsx
@@ -1,5 +1,5 @@
 import { VStack, Menu, Button, useColorMode, StatusBar } from 'native-base'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MenuIcon from 'react-native-vector-icons/Ionicons'
 import { MenuItem } from '../MenuItem'
 
@@ -9,91 +9,88 @@ export function MenuRight() {
 	const [shouldOverlapWithTrigger] = useState(false)
 	const { colorMode, toggleColorMode } = useColorMode()
 
-	function changeColorApp() {
-		toggleColorMode()
-		return (
+	useEffect(() => { }, [colorMode])
+
+	return (
+		<>
+			<VStack
+				space={6}
+			>
+				<Menu
+					w='200'
+					rounded={'lg'}
+					shouldOverlapWithTrigger={shouldOverlapWithTrigger} // @ts-ignore
+					placement={'bottom right'}
+					backgroundColor={'gray.100'}
+					_dark={{
+						backgroundColor: 'blueGray.700'
+					}}
+					trigger={(triggerProps) => {
+						return (
+							<Button
+								alignSelf='center'
+								variant='unstyled'
+								colorScheme={'blue'}
+								{...triggerProps}
+							>
+								<MenuIcon
+									name='menu'
+									size={25}
+									color={
+										colorMode === 'dark' ? 'white' : 'black'
+									}
+								/>
+							</Button>
+						)
+					}}
+				>
+					<Menu.Item
+						onPress={toggleColorMode}
+					>
+						<MenuItem
+							iconName={
+								colorMode === 'dark' ? 'sun' : 'moon'
+							}
+							title='Change  theme'
+							color={
+								colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
+							}
+						/>
+					</Menu.Item>
+					<Menu.Item >
+						<MenuItem
+							iconName='user'
+							title='Edit your Name'
+							color={
+								colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
+							}
+						/>
+					</Menu.Item>
+					<Menu.Item>
+						<MenuItem
+							iconName='edit'
+							title='Rename List'
+							color={
+								colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
+							}
+						/>
+					</Menu.Item>
+					<Menu.Item>
+						<MenuItem
+							iconName='power'
+							title='Log Out'
+							color={
+								colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
+							}
+						/>
+					</Menu.Item>
+
+				</Menu>
+			</VStack >
 			<StatusBar
 				translucent
 				barStyle={colorMode === 'dark' ? 'light-content' : 'dark-content'}
 			></StatusBar>
-		)
-
-	}
-
-	return (
-		<VStack
-			space={6}
-		>
-			<Menu
-				w='200'
-				rounded={'lg'}
-				shouldOverlapWithTrigger={shouldOverlapWithTrigger} // @ts-ignore
-				placement={'bottom right'}
-				backgroundColor={'gray.100'}
-				_dark={{
-					backgroundColor: 'blueGray.700'
-				}}
-				trigger={(triggerProps) => {
-					return (
-						<Button
-							alignSelf='center'
-							variant='unstyled'
-							colorScheme={'blue'}
-							{...triggerProps}
-						>
-							<MenuIcon
-								name='menu'
-								size={25}
-								color={
-									colorMode === 'dark' ? 'white' : 'black'
-								}
-							/>
-						</Button>
-					)
-				}}
-			>
-				<Menu.Item
-					onPress={changeColorApp}
-				>
-					<MenuItem
-						iconName={
-							colorMode === 'dark' ? 'sun' : 'moon'
-						}
-						title='Change  theme'
-						color={
-							colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
-						}
-					/>
-				</Menu.Item>
-				<Menu.Item >
-					<MenuItem
-						iconName='user'
-						title='Edit your Name'
-						color={
-							colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
-						}
-					/>
-				</Menu.Item>
-				<Menu.Item>
-					<MenuItem
-						iconName='edit'
-						title='Rename List'
-						color={
-							colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
-						}
-					/>
-				</Menu.Item>
-				<Menu.Item>
-					<MenuItem
-						iconName='power'
-						title='Log Out'
-						color={
-							colorMode === 'dark' ? 'white' : 'rgb(75, 85, 99)'
-						}
-					/>
-				</Menu.Item>
-
-			</Menu>
-		</VStack >
+		</>
 	)
 }
