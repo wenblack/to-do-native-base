4f39061cd610c3a7fcad6f3476082eae4823ef29
Wender Barbosa <wenblack012@gmail.com>
2023-03-09 14:04:08 -0300

    Update files for web build

--- android/app/src/release/java/com/wenblack/todo/ReactNativeFlipper.java
+++ android/app/src/release/java/com/wenblack/todo/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.todo;
+package com.wenblack.todo;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
